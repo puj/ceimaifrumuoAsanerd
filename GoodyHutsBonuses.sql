85a6d9e02742624d52f7d88f6af133fc83fc7117
Andreea <andreea.bocancea@gmail.com>
2016-11-27 14:44:06 +0100

    change weights to goody huts bonuses

--- GoodyHutsBonuses.sql
+++ GoodyHutsBonuses.sql
@@ -6,7 +6,7 @@ INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent)
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
   VALUES ('GOODY_CULTURE_LARGE_MODIFIER', 'YieldType', 'ARGTYPE_IDENTITY', 'YIELD_CULTURE');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
-  VALUES ('GOODY_CULTURE_LARGE_MODIFIER', 'Amount', 'ARGTYPE_IDENTITY', '100');
+  VALUES ('GOODY_CULTURE_LARGE_MODIFIER', 'Amount', 'ARGTYPE_IDENTITY', '40');
 
 -- add spy bonus to goody huts
 INSERT INTO GoodyHutSubTypes (GoodyHut, SubTypeGoodyHut, Description, Weight, ModifierID)
@@ -42,84 +42,84 @@ INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
 
 -- add great merchant points bonus to goody huts
 INSERT INTO GoodyHutSubTypes (GoodyHut, SubTypeGoodyHut, Description, Weight, ModifierID)
-  VALUES ('GOODYHUT_GOLD', 'GOODYHUT_GREAT_MERCHANT_POINTS', '15 GREAT MERCHANT POINTS BONUS', '15', 'GOODY_GOLD_GREAT_MERCHANT_POINTS');
+  VALUES ('GOODYHUT_GOLD', 'GOODYHUT_GREAT_MERCHANT_POINTS', '1 GREAT MERCHANT POINTS', '5', 'GOODY_GOLD_GREAT_MERCHANT_POINTS');
 INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent)
   VALUES ('GOODY_GOLD_GREAT_MERCHANT_POINTS', 'MODIFIER_PLAYER_ADJUST_GREAT_PERSON_POINTS', '1', '1');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
   VALUES ('GOODY_GOLD_GREAT_MERCHANT_POINTS', 'GreatPersonClassType', 'ARGTYPE_IDENTITY', 'GREAT_PERSON_CLASS_MERCHANT');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
-  VALUES ('GOODY_GOLD_GREAT_MERCHANT_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '15');
+  VALUES ('GOODY_GOLD_GREAT_MERCHANT_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '1');
 
 -- add great engineer points bonus to goody huts
 INSERT INTO GoodyHutSubTypes (GoodyHut, SubTypeGoodyHut, Description, Weight, ModifierID)
-  VALUES ('GOODYHUT_MILITARY', 'GOODYHUT_GREAT_ENGINEER_POINTS', '15 GREAT ENGINEER POINTS BONUS', '15', 'GOODY_GREAT_ENGINEER_POINTS');
+  VALUES ('GOODYHUT_MILITARY', 'GOODYHUT_GREAT_ENGINEER_POINTS', '1 GREAT ENGINEER POINTS', '10', 'GOODY_GREAT_ENGINEER_POINTS');
 INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent)
   VALUES ('GOODY_GREAT_ENGINEER_POINTS', 'MODIFIER_PLAYER_ADJUST_GREAT_PERSON_POINTS', '1', '1');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
   VALUES ('GOODY_GREAT_ENGINEER_POINTS', 'GreatPersonClassType', 'ARGTYPE_IDENTITY', 'GREAT_PERSON_CLASS_ENGINEER');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
-  VALUES ('GOODY_GREAT_ENGINEER_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '15');
+  VALUES ('GOODY_GREAT_ENGINEER_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '1');
 
 -- add great general points bonus to goody huts
 INSERT INTO GoodyHutSubTypes (GoodyHut, SubTypeGoodyHut, Description, Weight, ModifierID)
-  VALUES ('GOODYHUT_MILITARY', 'GOODYHUT_GREAT_GENERAL_POINTS', '15 GREAT GENERAL POINTS BONUS', '15', 'GOODY_GREAT_GENERAL_POINTS');
+  VALUES ('GOODYHUT_MILITARY', 'GOODYHUT_GREAT_GENERAL_POINTS', '1 GREAT GENERAL POINTS', '5', 'GOODY_GREAT_GENERAL_POINTS');
 INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent)
   VALUES ('GOODY_GREAT_GENERAL_POINTS', 'MODIFIER_PLAYER_ADJUST_GREAT_PERSON_POINTS', '1', '1');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
   VALUES ('GOODY_GREAT_GENERAL_POINTS', 'GreatPersonClassType', 'ARGTYPE_IDENTITY', 'GREAT_PERSON_CLASS_GENERAL');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
-  VALUES ('GOODY_GREAT_GENERAL_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '15');
+  VALUES ('GOODY_GREAT_GENERAL_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '1');
 
 -- add great prophet points bonus to goody huts
 INSERT INTO GoodyHutSubTypes (GoodyHut, SubTypeGoodyHut, Description, Weight, ModifierID)
-  VALUES ('GOODYHUT_FAITH', 'GOODYHUT_GREAT_PROPHET_POINTS', '20 GREAT PROPHET POINTS BONUS', '10', 'GOODY_FAITH_GREAT_PROPHET_POINTS');
+  VALUES ('GOODYHUT_FAITH', 'GOODYHUT_GREAT_PROPHET_POINTS', '1 GREAT PROPHET POINTS', '5', 'GOODY_FAITH_GREAT_PROPHET_POINTS');
 INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent)
   VALUES ('GOODY_FAITH_GREAT_PROPHET_POINTS', 'MODIFIER_PLAYER_ADJUST_GREAT_PERSON_POINTS', '1', '1');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
   VALUES ('GOODY_FAITH_GREAT_PROPHET_POINTS', 'GreatPersonClassType', 'ARGTYPE_IDENTITY', 'GREAT_PERSON_CLASS_PROPHET');
 INSERT INTO ModifierArguments (ModifierId, Name, Type, Value)
-  VALUES ('GOODY_FAITH_GREAT_PROPHET_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '20');
+  VALUES ('GOODY_FAITH_GREAT_PROPHET_POINTS', 'Amount', 'ARGTYPE_IDENTITY', '1');
 
 -- Culture
-UPDATE GoodyHutSubTypes SET Weight = '25' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_RELIC';
-UPDATE GoodyHutSubTypes SET Weight = '30' WHERE SubTypeGoodyHut = 'GOODYHUT_TWO_CIVIC_BOOSTS';
-UPDATE GoodyHutSubTypes SET Weight = '55' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_CIVIC_BOOST';
-UPDATE GoodyHutSubTypes SET Weight = '40' WHERE SubTypeGoodyHut = 'GOODHUT_LARGE_CULTURE';
+UPDATE GoodyHutSubTypes SET Weight = '20' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_RELIC';
+UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODYHUT_TWO_CIVIC_BOOSTS';
+UPDATE GoodyHutSubTypes SET Weight = '45' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_CIVIC_BOOST';
+UPDATE GoodyHutSubTypes SET Weight = '20' WHERE SubTypeGoodyHut = 'GOODHUT_LARGE_CULTURE';
 
 -- Faith
-UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODYHUT_LARGE_FAITH';
+UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODYHUT_LARGE_FAITH';
 UPDATE GoodyHutSubTypes SET Weight = '55' WHERE SubTypeGoodyHut = 'GOODYHUT_MEDIUM_FAITH';
 UPDATE GoodyHutSubTypes SET Weight = '30' WHERE SubTypeGoodyHut = 'GOODYHUT_SMALL_FAITH';
-UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_PROPHET_POINTS';
+UPDATE GoodyHutSubTypes SET Weight = '5' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_PROPHET_POINTS';
 
 -- Gold
 UPDATE GoodyHutSubTypes SET Weight = '55' WHERE SubTypeGoodyHut = 'GOODYHUT_LARGE_GOLD';
-UPDATE GoodyHutSubTypes SET Weight = '30' WHERE SubTypeGoodyHut = 'GOODYHUT_MEDIUM_GOLD';
-UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODYHUT_SMALL_GOLD';
-UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_MERCHANT_POINTS';
+UPDATE GoodyHutSubTypes SET Weight = '25' WHERE SubTypeGoodyHut = 'GOODYHUT_MEDIUM_GOLD';
+UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODYHUT_SMALL_GOLD';
+UPDATE GoodyHutSubTypes SET Weight = '5' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_MERCHANT_POINTS';
 UPDATE GoodyHutSubTypes SET Weight = '5' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_SPY';
 
 -- Military
-UPDATE GoodyHutSubTypes SET Weight = '40' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_SCOUT';
-UPDATE GoodyHutSubTypes SET Weight = '40' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_UPGRADE';
-UPDATE GoodyHutSubTypes SET Weight = '5' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_EXPERIENCE';
+UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_SCOUT';
+UPDATE GoodyHutSubTypes SET Weight = '30' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_UPGRADE';
+UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_EXPERIENCE';
 UPDATE GoodyHutSubTypes SET Weight = '0' WHERE SubTypeGoodyHut = 'GOODYHUT_HEAL';
 UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODHUT_GRANT_RANGED_UNIT';
 UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODHUT_GRANT_MELEE_UNIT';
 UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODHUT_GRANT_LIGHT_CAVALRY_UNIT';
-UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_ENGINEER_POINTS';
-UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_GENERAL_POINTS';
+UPDATE GoodyHutSubTypes SET Weight = '5' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_ENGINEER_POINTS';
+UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODHUT_GREAT_GENERAL_POINTS';
 
 -- Science
-UPDATE GoodyHutSubTypes SET Weight = '55' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_TECH';
+UPDATE GoodyHutSubTypes SET Weight = '30' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_TECH';
 UPDATE GoodyHutSubTypes SET Weight = '30' WHERE SubTypeGoodyHut = 'GOODYHUT_TWO_TECH_BOOSTS';
-UPDATE GoodyHutSubTypes SET Weight = '55' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_TECH_BOOST';
+UPDATE GoodyHutSubTypes SET Weight = '40' WHERE SubTypeGoodyHut = 'GOODYHUT_ONE_TECH_BOOST';
 
 -- Survivors
-UPDATE GoodyHutSubTypes SET Weight = '40' WHERE SubTypeGoodyHut = 'GOODYHUT_ADD_POP';
-UPDATE GoodyHutSubTypes SET Weight = '35' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_BUILDER';
-UPDATE GoodyHutSubTypes SET Weight = '20' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_TRADER';
-UPDATE GoodyHutSubTypes SET Weight = '20' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_SETTLER';
+UPDATE GoodyHutSubTypes SET Weight = '45' WHERE SubTypeGoodyHut = 'GOODYHUT_ADD_POP';
+UPDATE GoodyHutSubTypes SET Weight = '25' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_BUILDER';
+UPDATE GoodyHutSubTypes SET Weight = '10' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_TRADER';
+UPDATE GoodyHutSubTypes SET Weight = '15' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_SETTLER';
 UPDATE GoodyHutSubTypes SET Weight = '5' WHERE SubTypeGoodyHut = 'GOODYHUT_GRANT_SPY';
 
 
